107515329abb8d9ef130261a5dbb75ee5ea95b09
enriqueedelberto <enriqueviiireyes@gmail.com>
2020-09-16 20:59:54 -0500

    Add Readme.md

---
+++ Arrays_PreparationKit/HourGlass.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays_PreparationKit
+{
+    public class HourGlass
+    {
+        private readonly int[,] matrix;
+        public HourGlass(int[,] _matrix)
+        {
+            this.matrix = _matrix;
+
+        }
+        private int hourGlassSumIndependent(int[,] arr)
+        {
+            int sum = 0;
+            for (int row = 0; row < 3; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    if (row % 2 == 0)
+                    {
+                        sum += arr[row, column];
+                        continue;
+                    }
+                    if ((row + column) % 2 == 0)
+                    {
+                        sum += arr[row, column];
+                        continue;
+                    }
+                }
+
+            }
+
+
+            return sum;
+        }
+
+        public int hourglassSumAdvance( )
+        {
+            int sumMaxHourGlass = 0;
+            int[] hourGlasses = new int[16];
+            int countHourGlases = 0;
+            for (int f = 0; f < 6; f++)
+            {
+
+                int maxLengthFila = f + 3;
+                if (maxLengthFila > 6) break;
+
+                for (int c = 0; c < 6; c++)
+                {
+                    int maxLengthColumn = c + 3;
+                    if (maxLengthColumn > 6) break;
+
+                    // while (maxLength <= arr[f].Length)  {
+                    int fIntern = 0;
+                    int cIntern = 0;
+                    int sum = 0;
+
+                    int rowOngoing = 0;
+                    int columnOngoing = 0;
+
+                    int[,] arrHandleHourGlassIndividually = new int[3, 3];
+
+                    for (fIntern = f; fIntern < maxLengthFila; fIntern++)
+                    {
+                        columnOngoing = 0;
+                        for (cIntern = c; cIntern < maxLengthColumn; cIntern++)
+                        {
+                            Console.WriteLine("arrGeneral - [{0},{1}] : {2}", fIntern, cIntern, this.matrix[fIntern, cIntern]);
+                            //se valida si la fila es Par.
+
+                            arrHandleHourGlassIndividually[rowOngoing, columnOngoing] = this.matrix[fIntern, cIntern];
+
+                            columnOngoing++;
+                             
+                        }
+
+                        rowOngoing++; 
+
+                    }
+                    sum = hourGlassSumIndependent(arrHandleHourGlassIndividually);
+
+                    // maxLength += 3;
+                    hourGlasses[countHourGlases] = sum;
+                    Console.WriteLine("hourSumGlasses - [{0}] : {1}", countHourGlases, sum);
+
+                    if (countHourGlases == 0)
+                    {
+                        sumMaxHourGlass = sum;
+                    }
+                    else if (sumMaxHourGlass < sum)
+                    {
+                        sumMaxHourGlass = sum;
+                    }
+
+                    countHourGlases++;
+
+                     
+
+                }
+
+
+            }
+            return sumMaxHourGlass;
+        }
+
+        // Complete the hourglassSum function below.
+        static  int hourglassSum(int[][] arr)
+        {
+            int sumMaxHourGlass = 0;
+            int[] hourGlasses = new int[16];
+            int countHourGlases = 0;
+            for (int f = 0; f < 6; f++)
+            {
+
+                int maxLengthFila = f + 3;
+                if (maxLengthFila > 6) break;
+
+                for (int c = 0; c < 6; c++)
+                {
+                    int maxLengthColumn = c + 3;
+                    if (maxLengthColumn > 6) break;
+                    int fIntern = 0;
+                    int cIntern = 0;
+                    int sum = 0;
+
+                    int rowOngoing = 0;
+                    int columnOngoing = 0;
+                    int[,] arrHandleHourGlassIndividually = new int[3, 3];
+
+
+                    for (fIntern = f; fIntern < maxLengthFila; fIntern++)
+                    {
+                        columnOngoing = 0;
+                        for (cIntern = c; cIntern < maxLengthColumn; cIntern++)
+                        {
+                            arrHandleHourGlassIndividually[rowOngoing, columnOngoing] = arr[fIntern][cIntern];
+                            columnOngoing++;
+
+                        }
+
+                        rowOngoing++;
+
+                    }
+
+                    sum = hourGlassSumI(arrHandleHourGlassIndividually);
+                    hourGlasses[countHourGlases] = sum;
+                    countHourGlases++;
+                    if (countHourGlases == 0)
+                    {
+                        sumMaxHourGlass = sum;
+                    }
+                    else if (sumMaxHourGlass < sum)
+                    {
+                        sumMaxHourGlass = sum;
+                    }
+
+
+
+                }
+
+
+            }
+            return sumMaxHourGlass;
+        }
+
+
+        /**
+         HourGlass sum for former approach
+             */
+        static int hourGlassSumI(int[,] arr)
+        {
+            int sum = 0;
+            for (int row = 0; row < 3; row++)
+            {
+                for (int column = 0; column < 3; column++)
+                {
+                    if (row % 2 == 0)
+                    {
+                        sum += arr[row,column];
+                        continue;
+                    }
+                    if ((row + column) % 2 == 0)
+                    {
+                        sum += arr[row,column];
+                        continue;
+                    }
+                }
+
+            }
+
+
+            return sum;
+        }
+    }
+}
--- Arrays_PreparationKit/Program.cs
+++ Arrays_PreparationKit/Program.cs
@@ -7,6 +7,105 @@ namespace Arrays_PreparationKit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            int[,] arr1 = new int[6, 6];
+            int[,] arr2 = new int[6, 6];
+            Console.Write("Input elements in the matrix :\n");
+
+
+            arr1[0, 0] = 1;
+            arr1[0, 1] = 1;
+            arr1[0, 2] = 1;
+            arr1[0, 3] = 0;
+            arr1[0, 4] = 0;
+            arr1[0, 5] = 0;
+
+
+            arr1[1, 0] = 0;
+            arr1[1, 1] = 1;
+            arr1[1, 2] = 0;
+            arr1[1, 3] = 0;
+            arr1[1, 4] = 0;
+            arr1[1, 5] = 0;
+
+            arr1[2, 0] = 1;
+            arr1[2, 1] = 1;
+            arr1[2, 2] = 1;
+            arr1[2, 3] = 0;
+            arr1[2, 4] = 0;
+            arr1[2, 5] = 0;
+
+            arr1[3, 0] = 0;
+            arr1[3, 1] = 0;
+            arr1[3, 2] = 2;
+            arr1[3, 3] = 4;
+            arr1[3, 4] = 4;
+            arr1[3, 5] = 0;
+
+            arr1[4, 0] = 0;
+            arr1[4, 1] = 0;
+            arr1[4, 2] = 0;
+            arr1[4, 3] = 2;
+            arr1[4, 4] = 0;
+            arr1[4, 5] = 0;
+
+            arr1[5, 0] = 0;
+            arr1[5, 1] = 0;
+            arr1[5, 2] = 1;
+            arr1[5, 3] = 2;
+            arr1[5, 4] = 4;
+            arr1[5, 5] = 0;
+
+
+            //another array
+            arr2[0, 0] = -1;
+            arr2[0, 1] = -1;
+            arr2[0, 2] = 0;
+            arr2[0, 3] = -9;
+            arr2[0, 4] = -2;
+            arr2[0, 5] = -2;
+
+
+            arr2[1, 0] = -2;
+            arr2[1, 1] = -1;
+            arr2[1, 2] = -6;
+            arr2[1, 3] = -8;
+            arr2[1, 4] = -2;
+            arr2[1, 5] = -5;
+
+            arr2[2, 0] = -1;
+            arr2[2, 1] = -1;
+            arr2[2, 2] = -1;
+            arr2[2, 3] = -2;
+            arr2[2, 4] = -3;
+            arr2[2, 5] = -4;
+
+            arr2[3, 0] = -1;
+            arr2[3, 1] = -9;
+            arr2[3, 2] = -2;
+            arr2[3, 3] = -4;
+            arr2[3, 4] = -4;
+            arr2[3, 5] = -5;
+
+            arr2[4, 0] = -7;
+            arr2[4, 1] = -3;
+            arr2[4, 2] = -3;
+            arr2[4, 3] = -2;
+            arr2[4, 4] = -9;
+            arr2[4, 5] = -9;
+
+            arr2[5, 0] = -1;
+            arr2[5, 1] = -3;
+            arr2[5, 2] = -1;
+            arr2[5, 3] = -2;
+            arr2[5, 4] = -4;
+            arr2[5, 5] = -5;
+
+            var hourGlass = new HourGlass(arr2);
+            var result = hourGlass.hourglassSumAdvance(); 
+
+
+            Console.WriteLine("The result is " + result);
         }
     }
 }
